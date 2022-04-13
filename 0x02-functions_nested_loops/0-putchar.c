780e6755f0f06f322d3aa94c0f432c83d9c2f824
ajayfrizzy <ajayholuwaseun@gmail.com>
2022-04-13 15:30:18 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ * */
+int main(void)
+{
+	char ermias[] = "_purchar";
+	int c;
+	for (c = o; c < 8; c++)
+	{	_purchar(ermias[c]);
+	}
+	_purchar('\n');
+	return (0)
+}
